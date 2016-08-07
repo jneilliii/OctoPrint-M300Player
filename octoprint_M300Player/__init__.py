2022e1b1e1d210fdc050f3464a3bbc0db54f64d7
jneilliii <jneilliii+github@gmail.com>
2016-08-07 15:19:30 -0400

    Initial release.

---
+++ octoprint_M300Player/__init__.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+
+import octoprint.plugin
+
+class M300Player(octoprint.plugin.AssetPlugin):
+	def PlayM300(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
+		if gcode and gcode.startswith("M300"):
+			self._plugin_manager.send_plugin_message(self._identifier, dict(type="beep", freq=gcode.split()[1].replace("S",""),duration=gcode.split()[2].replace("P","")))
+			return
+			
+	def get_assets(self):
+		return dict(js=["js/M300Player.js"])
+		
+	def get_version(self):
+		return self._plugin_version
+		
+	##~~ Softwareupdate hook
+	def get_update_information(self):
+		return dict(
+			M300Player=dict(
+				displayName="M300Player",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="jneilliii",
+				repo="OctoPrint-M300Player",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/jneilliii/OctoPrint-M300Player/archive/{target_version}.zip"
+			)
+		)
+
+__plugin_name__ = "M300Player"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = M300Player()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.PlayM300,
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
