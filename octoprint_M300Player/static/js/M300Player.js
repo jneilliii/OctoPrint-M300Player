27b96e0d72cee49f05c2759319e50a0f790f53e8
jneilliii <jneilliii+github@gmail.com>
2016-09-03 18:48:27 -0400

    removing console debugging lines.

--- octoprint_M300Player/static/js/M300Player.js
+++ octoprint_M300Player/static/js/M300Player.js
@@ -76,15 +76,8 @@ $(function() {
 			if(data.type == "beep") {
 				self.notesBuffer.push([parseInt(data.freq.replace("S","")),parseInt(data.duration.replace("P",""))]); //push frequency,duration values into array for processing
 				if (self.audioCtx.state === "suspended") {
-					console.log(self.waveType()+':'+self.gainLevel());
 					self.playNotes();
 				}
-			}
-			
-			if (data.type == "updateSettings") {
-				console.log(self.waveType()+':'+self.gainLevel());
-				self.oscillator.type = self.waveType();
-				self.gainNode.gain.value = parseFloat(self.gainLevel());
 			}
 		}
 		
@@ -99,7 +92,6 @@ $(function() {
 		}
 		
 		self.onEventSettingsUpdated = function (payload) {
-			console.log(self.settingsViewModel.settings.plugins.M300Player.waveType() + '|' + self.settingsViewModel.settings.plugins.M300Player.gainLevel());
             self.oscillator.type = self.settingsViewModel.settings.plugins.M300Player.waveType();
 			self.gainNode.gain.value = self.settingsViewModel.settings.plugins.M300Player.gainLevel();
         }
